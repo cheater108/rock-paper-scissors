16fefee91a4ff190eb5ab4e079865d3f80040985
Chetan Anand <chetan108@outlook.com>
2024-06-25 22:59:13 +0530

    beta1 added all desired functionality

--- script.js
+++ script.js
@@ -1,46 +1,36 @@
 const container = document.querySelector("#container");
-const playArea = document.querySelector("#play-area");
 
-const result = `<div class="result">
-                    <div class="result-left">
-                        <p>YOU PICKED</p>
-                        <div class="action rock">
-                            <div class="action-inside">
-                                <img src="assets/rock.png" alt="" />
-                            </div>
-                        </div>
-                    </div>
-                    <div class="result-middle">
-                        <p>YOU LOST</p>
-                        <p>AGAINST PC</p>
-                        <button>PLAY AGAIN</button>
-                    </div>
-                    <div class="result-right">
-                        <p>PC PICKED</p>
-                        <div class="action paper">
-                            <div class="action-inside">
-                                <img src="assets/paper.png" alt="" />
-                            </div>
-                        </div>
-                    </div>
-                    <div class="concentric-container">
-                        <div class="circle3">
-                            <div class="circle2">
-                                <div class="circle1"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>`;
+function getPlayerScore() {
+    if (localStorage.getItem("playerScore_game")) {
+        return localStorage.getItem("playerScore_game");
+    } else {
+        localStorage.setItem("playerScore_game", 0);
+        return 0;
+    }
+}
 
-const winningScreen = `<div class="winner-screen">
-                <div class="stars-trophy">
-                    <img src="assets/stars.png" alt="" />
-                    <img class="trophy" src="assets/trophy.png" alt="" />
-                </div>
-                <p>HURRAY!!</p>
-                <p>YOU WON THE GAME</p>
-                <button class="play-again">PLAY AGAIN</button>
-            </div>`;
+function getPcScore() {
+    if (localStorage.getItem("pcScore_game")) {
+        return localStorage.getItem("pcScore_game");
+    } else {
+        localStorage.setItem("pcScore_game", 0);
+        return 0;
+    }
+}
+
+function increasePlayerScore() {
+    localStorage.setItem(
+        "playerScore_game",
+        Number(localStorage.getItem("playerScore_game")) + 1
+    );
+}
+
+function increasePcScore() {
+    localStorage.setItem(
+        "pcScore_game",
+        Number(localStorage.getItem("pcScore_game")) + 1
+    );
+}
 
 function initializeStructure() {
     const initialStructure = `<div class="header">
@@ -55,14 +45,14 @@ function initializeStructure() {
                             COMPUTER <br />
                             SCORE
                         </p>
-                        <p id="computer-score">0</p>
+                        <p id="computer-score">${getPcScore()}</p>
                     </div>
                     <div class="score">
                         <p>
                             YOUR <br />
                             SCORE
                         </p>
-                        <p id="your-score">0</p>
+                        <p id="your-score">${getPlayerScore()}</p>
                     </div>
                 </div>
             </div>
@@ -98,7 +88,7 @@ function initializeStructure() {
     container.innerHTML = initialStructure;
 }
 
-function initializeGame() {
+function initializeRulesBtn() {
     const rulesBtn = document.querySelector("#rules-btn");
     const rulesClose = document.querySelector("#rulesClose");
     const rules = document.querySelector("#rules");
@@ -111,7 +101,162 @@ function initializeGame() {
     });
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    //initializeStructure();
+function initializePlayAgainBtn() {
+    const playAgain = document.querySelector(".play-again");
+    playAgain.addEventListener("click", () => {
+        initializeStructure();
+        initializeGame();
+        initializeNextBtn();
+    });
+}
+
+function nextBtnHandler() {
+    renderWinningScreen();
+    initializePlayAgainBtn();
+    initializeNextBtn();
+}
+function initializeNextBtn(result) {
+    const next = document.querySelector("#next");
+    if (result === "WON") {
+        next.classList.remove("hidden");
+        next.addEventListener("click", nextBtnHandler, false);
+    } else {
+        // removing click event listner before adding event listner
+        // so that only one event listner is added of next btn.
+        next.removeEventListener("click", nextBtnHandler, false);
+        next.classList.add("hidden");
+    }
+}
+
+function initializeGame() {
+    const rock = document.querySelector("#rock");
+    const paper = document.querySelector("#paper");
+    const scissors = document.querySelector("#scissors");
+
+    rock.addEventListener("click", () => {
+        const pc = pcMove();
+        const result = wonOrLostOrTie("rock", pc);
+        renderResult("rock", pc, result);
+        initializePlayAgainBtn();
+        initializeNextBtn(result);
+    });
+
+    paper.addEventListener("click", () => {
+        const pc = pcMove();
+        const result = wonOrLostOrTie("paper", pc);
+        renderResult("paper", pc, result);
+        initializePlayAgainBtn();
+        initializeNextBtn(result);
+    });
+
+    scissors.addEventListener("click", () => {
+        const pc = pcMove();
+        const result = wonOrLostOrTie("scissors", pc);
+        renderResult("scissors", pc, result);
+        initializePlayAgainBtn();
+        initializeNextBtn(result);
+    });
+}
+
+function wonOrLostOrTie(user, pc) {
+    const resultObj = {
+        rock: {
+            scissors: "WON",
+            paper: "LOST",
+        },
+        paper: {
+            scissors: "LOST",
+            rock: "WON",
+        },
+        scissors: {
+            rock: "LOST",
+            paper: "WON",
+        },
+    };
+    if (user === pc) {
+        return false;
+    }
+    const result = resultObj[user][pc];
+    if (result === "WON") {
+        increasePlayerScore();
+    } else if (result === "LOST") {
+        increasePcScore();
+    }
+    return result;
+}
+
+function pcMove() {
+    const move = ["rock", "paper", "scissors"];
+    const pcMoveInd = Math.floor(Math.random() * 3 * 3) % 3;
+    return move[pcMoveInd];
+}
+
+function getResultScreen(userMove, pcMove, result) {
+    const resultScreen = `<div class="result">
+    <div class="result-left">
+        <p>YOU PICKED</p>
+        <div class="action ${userMove}">
+            <div class="action-inside">
+                <img src="assets/${userMove}.png" alt="" />
+            </div>
+        </div>
+    </div>
+    <div class="result-middle">
+        <p>${result ? "YOU " + result : "TIE UP"}</p>
+        <p>${result ? "AGAINST PC" : "&nbsp;"}</p>
+        <button class='play-again'>PLAY AGAIN</button>
+    </div>
+    <div class="result-right">
+        <p>PC PICKED</p>
+        <div class="action ${pcMove}">
+            <div class="action-inside">
+                <img src="assets/${pcMove}.png" alt="" />
+            </div>
+        </div>
+    </div>
+    <div class="concentric-container ${
+        result ? "" : "hidden"
+    }" style="top: -36px; left: ${result === "WON" ? "-4px" : "638px"};">
+        <div class="circle3">
+            <div class="circle2">
+                <div class="circle1"></div>
+            </div>
+        </div>
+    </div>
+</div>`;
+
+    return resultScreen;
+}
+
+function renderWinningScreen() {
+    const winningScreen = `<div class="winner-screen">
+    <div class="stars-trophy">
+        <img src="assets/stars.png" alt="" />
+        <img class="trophy" src="assets/trophy.png" alt="" />
+    </div>
+    <p>HURRAY!!</p>
+    <p>YOU WON THE GAME</p>
+    <button class="play-again">PLAY AGAIN</button>
+</div>`;
+    container.innerHTML = winningScreen;
+}
+
+function renderResult(userMove, pcMove, result) {
+    const playArea = document.querySelector("#play-area");
+    const playerScore = document.querySelector("#your-score");
+    const pcScore = document.querySelector("#computer-score");
+
+    playerScore.textContent = getPlayerScore();
+    pcScore.textContent = getPcScore();
+    playArea.innerHTML = getResultScreen(userMove, pcMove, result);
+}
+
+function initializeApp() {
+    initializeStructure();
+    initializeRulesBtn();
     initializeGame();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    initializeApp();
 });
